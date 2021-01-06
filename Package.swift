7eaf6a9741e1a2bd698c4dfc4184cfa138f2aba3
MapTiler <sysadmin@maptiler.com>
2021-01-06 19:50:34 +0100

    New release: 5.10.0

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.9.0/Mapbox-5.9.0.zip",
-            checksum: "ee0dc76aadf19a43df658e34fa6ce8c82a4c35d059d42621907f303e53724046"),
+            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.10.0/Mapbox-5.10.0.zip",
+            checksum: "edcca3b7b84ca50eeabe28e1a2d9ba3e34cee2ca7e8daa8ad9e67393457ed9f3"),
         .binaryTarget(
             name: "MapboxMobileEvents",
-            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.9.0/MapboxMobileEvents-5.9.0.zip",
-            checksum: "a96842857465adc2f2f3eeaa94dff25e58875bddd871e1a9edeef325591d111a")            
+            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.10.0/MapboxMobileEvents-5.10.0.zip",
+            checksum: "78298810dc3c3370ad2cf8265b1e6ef3fc406565c447a5275d689a5f06cedfd9")            
     ]
 )
