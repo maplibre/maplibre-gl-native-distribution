1e3cda378293a42362c5c811f055aa131a8a0ea9
Petr Pokorny <petr-pokorny@hotmail.com>
2021-05-06 21:39:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,21 +6,14 @@ let package = Package(
     products: [
         .library(
             name: "Mapbox",
-            targets: ["Mapbox"]),
-        .library(
-            name: "MetalANGLE",
-            targets: ["MetalANGLE"])            
+            targets: ["Mapbox"])
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/maplibre/maplibre-gl-native/releases/download/ios-v5.12.0-pre.1/Mapbox-5.12.0-pre.1.zip",
-            checksum: "f559d1cb214b22427af4f2a2dc528bac4003b0fdeb239de0007ce4564535cc12"),
-        .binaryTarget(
-            name: "MetalANGLE",
-            url: "https://github.com/maplibre/maplibre-gl-native/releases/download/ios-v5.12.0-pre.1/MetalANGLE-5.12.0-pre.1.zip",
-            checksum: "0cdcb712dad68fcc3bf8eca457781fe1a0f783e4d7439efb5d9e5095c47c1e00")            
+            url: "https://github.com/maplibre/maplibre-gl-native/releases/download/ios-v5.11.0/Mapbox-5.11.0.zip",
+            checksum: "3446a5ed2f8cc144ec9a6e474d59d4f803e227bac78201f0f6494d7f7251cb77")
     ]
 )
