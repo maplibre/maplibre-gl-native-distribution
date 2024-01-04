71237cde41cad7b2af4df2abed637383e879fb2e
Bart Louwers <bart.louwers@gmail.com>
2024-01-04 13:55:56 +0100

    Add workflow_dispatch trigger for release (#24)
    * Add release workflow
    
    * Fix sed
    
    * configure git user
    
    * Push branch as well
    
    * Release 6.0.0-pre612b5dd8a9d4f15c9884a2280de88f3e9ba53073
    
    * Update Package.swift
    
    ---------
    
    Co-authored-by: GitHub Actions <git@maplibre.org>

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ let package = Package(
     name: "MapLibre Native",
     products: [
         .library(
-            name: "Mapbox",
-            targets: ["Mapbox"])
+            name: "MapLibre",
+            targets: ["MapLibre"])
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
-            name: "Mapbox",
+            name: "MapLibre",
             url: "https://github.com/maplibre/maplibre-native/releases/download/ios-v5.13.0/Mapbox-5.13.0.zip",
             checksum: "6f52b877c639c022f59984b346d0758cc7e34ac19d8b2110d0444df32bfe140f")
     ]
